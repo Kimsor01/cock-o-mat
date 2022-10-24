ed40cb1392821df7fcae3677852d03dd0b562a86
Kimsor01 <hannibal7471@gmail.com>
2022-10-24 23:58:40 +0200

    bottles list of dicts mixes list of dicts (bis S.2 fertig)

---
+++ booze.py
@@ -0,0 +1,118 @@
+bottles = [
+    # alcoholics
+    {"name": "Tequila", "value": "teq"},
+    {"name": "Cachaca", "value": "cacha"},
+    {"name": "Vodka", "value": "vodka"},
+    {"name": "Rum (weiß)", "value": "rum"},
+    {"name": "Pfirsichlikör", "value": "peach"},
+    {"name": "Blue Curacao", "value": "blue"},
+
+    # mixables
+    {"name": "Orangensaft", "value": "osaft"},
+    {"name": "Grenadine", "value": "grena"},
+    {"name": "Limettensaft", "value": "lime"},
+    {"name": "Ananassaft", "value": "pine"},
+    {"name": "Coca Cola", "value": "cola"},
+    {"name": "Cranberrysaft", "value": "cran"}
+]
+
+# ALL MIXES ARE TO BE TESTED FOR TASTE AND AMOUNT
+mixes = [
+    # ingredient amount in "ml"
+    {
+        "name": "A Kick in the Crotch",
+        "ingredients": {"vodka": 20, "blue": 15, "cran": 15},
+        "type": "Shooter"  # to be replaced with dumb german expression
+    },
+    {
+        "name": "Ananascocktail",
+        "ingredients": {"pine": 120, "osaft": 90},
+        "type": "Fahrer"
+    },
+    {
+        "name": "Rum Cola",
+        "ingredients": {"rum": 40, "cola": 160},
+        "type": "Langtrink"
+    },
+    {
+        "name": "Black Thunder",
+        "ingredients": {"vodka": 40, "blue": 20, "cola": 160},
+        "type": "Langtrink"
+    },
+    {
+        "name": "Blue Lagoon",
+        "ingredients": {"rum": 30, "blue": 20, "pine": 70},
+        "type": "SchwanzSchwanz"
+    },
+    {
+        "name": "Blue Tequila",
+        "ingredients":{"blue": 20, "teq": 20},
+        "type": "Schäpperer"
+    },
+    {
+        "name": "Cape Cod",
+        "ingredients": {"vodka": 40, "cran": 160},
+        "type": "Langtrink"
+    },
+    {
+        "name": "Vodka Cola",
+        "ingredients": {"vodka": 20, "cola": 180},
+        "type": "Langtrink"
+    },
+    {
+        "name": "Emerald Dream",
+        "ingredients": {"vodka": 50, "blue": 20, "osaft": 100},
+        "type": "Langtrink"
+    },
+    {
+        "name": "Finnish Virgin",
+        "ingredients": {"vodka": 50, "blue": 10, "osaft": 20, "lime": 10},
+        "type": "Langtrink"
+    },
+    {
+        "name": "Fireman's Sour",
+        "ingredients": {"rum": 40, "lime": 30, "grena": 15},
+        "type": "Sauerer"
+    },
+    {
+        "name": "Fuzzy Navel",
+        "ingredients": {"osaft": 150, "peach": 30, "vodka": 30},
+        "type": "Langtrink"
+    },
+    {
+        "name": "Green Devil",
+        "ingredients": {"blue": 10, "osaft": 20, "rum": 20},
+        "type": "SchwanzSchwanz"
+    },
+    {
+        "name": "Green Eyes",
+        "ingredients": {"vodka": 30, "blue": 30, "osaft": 120},
+        "type": "Langtrink"
+    },
+    {
+        "name": "Green Pineapple",
+        "ingredients": {"teq": 30, "pine": 60, "blue": 20},
+        "type": "SchwanzSchwanz"
+    },
+    {
+        "name": "Grüne Witwe",
+        "ingredients": {"blue": 40, "osaft": 160},
+        "type": "Langtrink"
+    },
+    {
+        "name": "Hund und Katze",
+        "ingredients": {"vodka": 40, "blue": 40, "rum": 40, "grena": 20, "lime": 20, "pine": 100, "osaft": 100},
+        "type": "SchwanzSchwanz"
+    },
+    {
+        "name": "Madras",
+        "ingredients": {"vodka": 50, "osaft": 150, "cran": 150},
+        "type": "Langtrink"
+    },
+    {
+        "name": "Mars Explosion",
+        "ingredients": {"grena": 10, "osaft": 100, "vodka": 20, "rum": 20},
+        "type": "Extravaganz"
+    }
+
+]
