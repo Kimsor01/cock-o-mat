56f15f53a0c922377f7e8036ab589c1b7159eb93
DG <hannibal7471@gmail.com>
2022-11-28 10:20:20 +0100

    filterMixes() Cockmixer - basics prep UI building

---
+++ booze_test.py
@@ -0,0 +1,61 @@
+mixes = [
+    {
+        "name": "A Kick in the Crotch",
+        "ingredients": {"vodka": 20, "blue": 15, "cran": 15},
+        "type": "Shooter",
+        "mixable": "True"
+    },
+    {
+        "name": "Ananascocktail",
+        "ingredients": {"pine": 120, "osaft": 90},
+        "type": "Fahrer",
+        "mixable": "True"
+    },
+    {
+        "name": "Rum Cola",
+        "ingredients": {"rum": 40, "cola": 160},
+        "type": "Langtrink",
+        "mixable": "True"
+    },
+    {
+        "name": "Black Thunder",
+        "ingredients": {"vodka": 40, "blue": 20, "cola": 160},
+        "type": "Langtrink",
+        "mixable": "True"
+    },
+    {
+        "name": "Blue Lagoon",
+        "ingredients": {"rum": 30, "blue": 20, "pine": 70},
+        "type": "SchwanzSchwanz",
+        "mixable": "True"
+    },
+    {
+        "name": "Blue Tequila",
+        "ingredients": {"blue": 20, "teq": 20},
+        "type": "Extravaganz",
+        "mixable": "True"
+    },
+    {
+        "name": "Cape Cod",
+        "ingredients": {"vodka": 40, "cran": 160},
+        "type": "Langtrink",
+        "mixable": "True"
+    },
+    {
+        "name": "Vodka Cola",
+        "ingredients": {"vodka": 20, "cola": 180},
+        "type": "Langtrink",
+        "mixable": "True"
+    },
+    {
+        "name": "Emerald Dream",
+        "ingredients": {"vodka": 50, "blue": 20, "osaft": 100},
+        "type": "Langtrink",
+        "mixable": "True"
+    },
+    {
+        "name": "Finnish Virgin",
+        "ingredients": {"vodka": 50, "blue": 10, "osaft": 20, "lime": 10},
+        "type": "Langtrink",
+        "mixable": "True"
+    }]
